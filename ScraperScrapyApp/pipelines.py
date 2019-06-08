b8aaacb4db4aacb3f89ebb9da380efa80eca4cc4
reaysawa <joao.paulo.silvasouza@hotmail.com>
2019-06-08 11:50:24 -0300

    pipeline for persisting results on the back of the Django ORM

--- ScraperScrapyApp/pipelines.py
+++ ScraperScrapyApp/pipelines.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from Portal import models as PortalDjangoAppModels
 
 
-class ScraperAppPipeline(object):
+class NewsHeadingPipeline(object):
+    items = []
+
+    def close_spider(self, spider):
+        """Stores every item in one go after crawling is done"""
+        if self.items:
+            PortalDjangoAppModels.NewsHeading.objects.bulk_create(self.items)
+
     def process_item(self, item, spider):
+        """Batches items to save SQL-query insertion times"""
+        self.items.append(item)
         return item
